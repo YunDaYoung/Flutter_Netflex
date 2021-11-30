d4bafa4d25f2040395d40a23af1f39d19178a40c
DESKTOP-1OJ603H\ydios <ydiosa98@naver.com>
2021-12-01 03:01:42 +0900

    #1,0 Navigation Tab

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+import 'package:netflex_flutter/widget/bottom_bar.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  late TabController controller;
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'netflex',
+      theme: ThemeData(
+          brightness: Brightness.dark,
+          primaryColor: Colors.black,
+          accentColor: Colors.white),
+      home: DefaultTabController(
+          length: 4,
+          child: Scaffold(
+            body: TabBarView(
+              physics: NeverScrollableScrollPhysics(),
+              children: <Widget>[
+                Container(
+                    child: const Center(
+                  child: Text('home'),
+                )),
+                Container(
+                    child: const Center(
+                  child: Text('search'),
+                )),
+                Container(
+                    child: const Center(
+                  child: Text('save'),
+                )),
+                Container(
+                    child: const Center(
+                  child: Text('more'),
+                ))
+              ],
+            ),
+            bottomNavigationBar: Bottom(),
+          )),
+    );
+    throw UnimplementedError();
+  }
+}
